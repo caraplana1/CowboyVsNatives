10c183b2f00303e78bf62b13643cf1b0a2be6b7f
Caraplana <cristianlandaez@gmail.com>
2021-05-19 12:30:02 -0400

    Limits Added to the player

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -8,10 +8,14 @@ public class PlayerController : Sprite
     private Vector2 direction;
     [Export]private float speed;
 
+
+    private Rect2 viewport;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
       area = GetChild<Area2D>(0);
+      viewport = GetViewportRect();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,28 +25,28 @@ public class PlayerController : Sprite
 
       direction = Vector2.Zero;
 
-      if (Input.IsActionPressed("move_up"))
+      if (Input.IsActionPressed("move_up") && Position.y > 0)
       {
           direction.y -= 1;
       }
-      if (Input.IsActionPressed("move_down"))
+      if (Input.IsActionPressed("move_down") && Position.y < viewport.Size.y)
       {
           direction.y += 1;
       }
-      if (Input.IsActionPressed("move_left"))
+      if (Input.IsActionPressed("move_left") && Position.x > 0)
       {
         direction.x -= 1;
       }
-      if (Input.IsActionPressed("move_right"))
+      if (Input.IsActionPressed("move_right") && Position.x < viewport.Size.x)
       {
           direction.x += 1;
       }
 
       direction = direction.Normalized();
       #endregion
-
+  
       Translate(direction * speed * delta);
-      
+            
   }
 
 }
